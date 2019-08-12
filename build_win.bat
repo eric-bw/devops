86c757c930c17b5a60f10cfe517d6214cfcaf3d1
Eric Talley <etalley@cydcor.com>
2019-08-12 08:41:32 -0700

    added a windows builder

--- build_win.bat
+++ build_win.bat
@@ -1,2 +1,5 @@
 pyinstaller migration_assistant.py --add-data "packagebuilder/wsdl/metadata-45.wsdl.xml;packagebuilder/wsdl" --onefile
 mv dist\migration_assistant.exe .
+git add migration_assistant.exe
+git commit -m "build snapshot"
+git push origin
